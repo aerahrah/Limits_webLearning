06a2212065d39c7179734b51d14d382d37f7c501
aerahrah <201910416@fit.edu.ph>
2023-03-24 12:39:31 +0800

    Refactor navigation bar and also added animation

---
+++ src/scripts/header-animation.js
@@ -0,0 +1,32 @@
+const nav_items = document.querySelectorAll('.nav-item')
+const lessonId = document.getElementById('lessons')
+const profileId= document.getElementById('profile')
+const quizId = document.getElementById('quiz')
+const aboutId = document.getElementById('about-us')
+const currentRoute = window.location.pathname.substring(1);
+
+function addActiveClass(route){
+    if(route == "lesson") {
+        removeActiveClasses()
+        lessonId.classList.add('active') 
+    }
+    if(route == "profile") {
+        removeActiveClasses()
+        profileId.classList.add('active') 
+    }
+    if(route == "quiz") {
+        removeActiveClasses()
+        quizId.classList.add('active') 
+    }
+    if(route == "about-us") {
+        removeActiveClasses()
+        aboutId.classList.add('active') 
+    }
+}
+addActiveClass(currentRoute)
+
+function removeActiveClasses(){
+    nav_items.forEach(item=>{
+        item.classList.remove('active')
+    })
+}
