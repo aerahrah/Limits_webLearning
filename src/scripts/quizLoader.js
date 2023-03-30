2c6beab25a74d2ca15acfeab60ad26288f62710f
aerahrah <201910416@fit.edu.ph>
2023-03-30 21:48:31 +0800

    Refactor quizloader.js and quiz.astro

--- src/scripts/quizLoader.js
+++ src/scripts/quizLoader.js
@@ -1,8 +1,9 @@
 const quizData = [
     {
         question: "Evaluate: ",
+        img: "/images/img_quiz/q1.png",
         answer: [
-        {img: '../assets/img_quiz/q3.png',a: "1", correct: false},
+        {a: "1", correct: false},
         {b: "2", correct: false},
         {c: "3", correct: true}, //C
         {d: "4", correct: false},
@@ -12,8 +13,9 @@ const quizData = [
     ,
     {
         question: "Evaluate: ",
+        img: "/images/img_quiz/q2.png",
         answer: [
-        {img: "questions/q2.png",a: "-∞", correct: true}, //A
+        {a: "-∞", correct: true}, //A
         {b: "+∞", correct: false},
         {c: "0", correct: false},
         {d: "dne", correct: false},
@@ -22,8 +24,9 @@ const quizData = [
     ,
     {
         question: "Evaluate: ",
+        img: "/images/img_quiz/q3.png",
         answer: [
-        {img: "questions/q3.png",a: "1/5", correct: false},
+        {a: "1/5", correct: false},
         {b: "2/5", correct: true}, //B
         {c: "3/5", correct: false},
         {d: "4/5", correct: false},
@@ -32,8 +35,9 @@ const quizData = [
     ,
     {
         question: "Find: ",
+        img: "/images/img_quiz/q4.png",
         answer: [
-        {img: "questions/q4.png",a: "2", correct: false},
+        {a: "2", correct: false},
         {b: "3", correct: false},
         {c: "4", correct: false},
         {d: "5", correct: true}, //D
@@ -42,8 +46,9 @@ const quizData = [
     ,
     {
         question: "Find: ",
+        img: "/images/img_quiz/q5.png",
         answer: [
-        {img: "questions/q5.png",a: "2", correct: false},
+        {a: "2", correct: false},
         {b: "3", correct: true}, //B
         {c: "4", correct: false},
         {d: "5", correct: false},
@@ -52,8 +57,9 @@ const quizData = [
     ,
     {
         question: "Evaluate: ",
+        img: "/images/img_quiz/q6.png",
         answer: [
-        {img: "questions/q6.png",a: "0", correct: true}, //A
+        {a: "0", correct: true}, //A
         {b: "1", correct: false},
         {c: "2", correct: false},
         {d: "3", correct: false},
@@ -62,8 +68,9 @@ const quizData = [
     ,
     {
         question: "Evaluate: ",
+        img: "/images/img_quiz/q7.png",
         answer: [
-        {img: "questions/q7.png",a: "5/4", correct: false}, 
+        {a: "5/4", correct: false}, 
         {b: "1/4", correct: true}, //B
         {c: "3/4", correct: false},
         {d: "7/4", correct: false},
@@ -72,8 +79,9 @@ const quizData = [
     ,
     {
         question: "Evaluate: ",
+        img: "/images/img_quiz/q8.png",
         answer: [
-        {img: "questions/q8.png",a: "-√7/4", correct: false},
+        {a: "-√7/4", correct: false},
         {b: "-√5/8", correct: false},
         {c: "-√11/6", correct: false},
         {d: "-√3/2", correct: true}, //D
@@ -82,8 +90,9 @@ const quizData = [
     ,
     {
         question: "Evaluate: ",
+        img: "/images/img_quiz/q9.png",
         answer: [
-        {img: "questions/q9.png",a: "1/3", correct: false},
+        {a: "1/3", correct: false},
         {b: "2/3", correct: true}, //B
         {c: "9/10", correct: false},
         {d: "9/5", correct: false},
@@ -92,8 +101,9 @@ const quizData = [
     ,
     {
         question: "Evaluate: ",
+        img: "/images/img_quiz/q10.png",
         answer: [
-        {img: "questions/q10.png",a: "1", correct: true}, //A
+        {a: "1", correct: true}, //A
         {b: "2", correct: false},
         {c: "3", correct: false},
         {d: "4", correct: false},
@@ -102,8 +112,9 @@ const quizData = [
     ,
     {
         question: "Find: ",
+        img: "/images/img_quiz/q11.png",
         answer: [
-        {img: "questions/q11.png",a: "0", correct: false},
+        {a: "0", correct: false},
         {b: "1", correct: false},
         {c: "2", correct: false},
         {d: "dne", correct: true}, //D
@@ -112,8 +123,9 @@ const quizData = [
     ,
     {
         question: "Find: ",
+        img: "/images/img_quiz/q12.png",
         answer: [
-        {img: "questions/q12.png",a: "dne", correct: false},
+        {a: "dne", correct: false},
         {b: "0", correct: true}, //B
         {c: "1", correct: false},
         {d: "2", correct: false},
@@ -122,8 +134,9 @@ const quizData = [
     ,
     {
         question: "Find: ",
+        img: "/images/img_quiz/q13.png",
         answer: [
-        {img: "questions/q13.png",a: "-1", correct: false},
+        {a: "-1", correct: false},
         {b: "0", correct: false},
         {c: "1", correct: true}, //C
         {d: "2", correct: false},
@@ -132,8 +145,9 @@ const quizData = [
     ,
     {
         question: "Find: ",
+        img: "/images/img_quiz/q14.png",
         answer: [
-        {img: "questions/q14.png",a: "-5", correct: false},
+        {a: "-5", correct: false},
         {b: "-4", correct: false},
         {c: "-3", correct: false},
         {d: "-2", correct: true}, //D
@@ -142,8 +156,9 @@ const quizData = [
     ,
     {
         question: "Find: ",
+        img: "/images/img_quiz/q15.png",
         answer: [
-        {img: "questions/q15.png",a: "3", correct: true}, //A
+        {a: "3", correct: true}, //A
         {b: "2", correct: false},
         {c: "1", correct: false},
         {d: "0", correct: false},
@@ -152,8 +167,9 @@ const quizData = [
     ,
     {
         question: "Evaluate: ",
+        img: "/images/img_quiz/q16.png",
         answer: [
-        {img: "questions/q16.png",a: "-5/6", correct: true}, //A
+        {a: "-5/6", correct: true}, //A
         {b: "-1/4", correct: false},
         {c: "-5/8", correct: false},
         {d: "-7/9", correct: false},
@@ -162,8 +178,9 @@ const quizData = [
     ,
     {
         question: "Evaluate: ",
+        img: "/images/img_quiz/q17.png",
         answer: [
-        {img: "questions/q17.png",a: "-∞", correct: false},
+        {a: "-∞", correct: false},
         {b: "+∞", correct: false},
         {c: "1", correct: false},
         {d: "dne", correct: true}, //D
@@ -172,8 +189,9 @@ const quizData = [
     ,
     {
         question: "Evaluate: ",
+        img: "/images/img_quiz/q18.png",
         answer: [
-        {img: "questions/q18.png",a: "-1/8", correct: true}, //A
+        {a: "-1/8", correct: true}, //A
         {b: "-3/8", correct: false},
         {c: "-5/8", correct: false},
         {d: "-7/8", correct: false},
@@ -182,8 +200,9 @@ const quizData = [
     ,
     {
         question: "Evaluate: ",
+        img: "/images/img_quiz/q19.png",
         answer: [
-        {img: "questions/q19.png",a: "3/7", correct: false},
+        {a: "3/7", correct: false},
         {b: "-3/7", correct: false},
         {c: "7/3", correct: true}, //C
         {d: "-7/3", correct: false},
@@ -192,8 +211,9 @@ const quizData = [
     ,
     {
         question: "Evaluate: ",
+        img: "/images/img_quiz/q20.png",
         answer: [
-        {img: "questions/q20.png",a: "1", correct: false},
+        {a: "1", correct: false},
         {b: "2", correct: false},
         {c: "3", correct: false},
         {d: "dne", correct: true}, //D
@@ -206,7 +226,7 @@ const quizData = [
 const quiz= document.getElementById('quiz')
 const answerEls = document.querySelectorAll('.answer')
 const quizContainer = document.getElementById("quiz-container");
-const questionEl = document.getElementById('question')
+const questionEl = document.querySelector('.quiz-container-header')
 const quizImage = document.getElementById("quiz-image");
 const a_text = document.getElementById('a_text')
 const b_text = document.getElementById('b_text')
@@ -221,15 +241,24 @@ let score = 0
 let currentQuestionIndex = -1;
 
 
+answerEls.forEach((answerEl) => {
+    answerEl.addEventListener('change', () => {
+      if(answerEl.checked) submitBtn.disabled = false;
+    //   const isAnswerChecked = Array.from(answerEls).some((answerEl) => answerEl.checked);
+      
+    });
+  });
+
 
 quizImage.src = quizData[0].answer[0].img;
 
 
 function loadQuiz() {
     deselectAnswers()
+    submitBtn.disabled = true;
     const currentQuizData = quizData[currentQuiz]
-    quizImage.src = currentQuizData.answer[0].img;
-    questionEl.innerText = currentQuizData.question
+    quizImage.src = currentQuizData.img;
+    questionEl.innerText = `Question ${currentQuiz+1} ${currentQuizData.question}`
     a_text.innerText = currentQuizData.answer[0].a
     b_text.innerText = currentQuizData.answer[1].b
     c_text.innerText = currentQuizData.answer[2].c
@@ -242,13 +271,10 @@ function deselectAnswers() {
 
 function getSelected() {
     let answer = null;
-  
+
     answerEls.forEach(answerEl => {
       if (answerEl.checked) {
         const answerId = answerEl.id;
-        console.log(answerId)
-        console.log(a[answerId])
-        console.log(a.correct)
         const isCorrect = quizData[currentQuiz].answer.some(a => a[answerId] && a.correct);
         
         if (isCorrect) {
@@ -269,6 +295,7 @@ function getSelected() {
 
 submitBtn.addEventListener('click', () => {
     const answer = getSelected()
+
     console.log(answer + "ksdlksjdlksjdjlskjd")
        if(answer === true) {
            score++
@@ -276,6 +303,9 @@ submitBtn.addEventListener('click', () => {
        currentQuiz++
        if(currentQuiz < quizData.length) {
            loadQuiz()
+           if(currentQuiz == (quizData.length-1)){
+                submitBtn.innerHTML = "See result";
+           }
        } 
        else {
            quiz.innerHTML = `
