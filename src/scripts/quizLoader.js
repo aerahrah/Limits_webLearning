ac4bab23a9e3abaf713a4bc63621205db9b254cd
Marc Ivan Guillermo <201910416@fit.edu.ph>
2023-04-01 13:03:18 +0800

    Remove array from quizLoader.js and Add quizData.json

--- src/scripts/quizLoader.js
+++ src/scripts/quizLoader.js
@@ -1,240 +1,25 @@
-const quizData = [
-    {
-        question: "Evaluate: ",
-        img: "/images/img_quiz/q1.png",
-        answer: [
-        {a: "1", correct: false},
-        {b: "2", correct: false},
-        {c: "3", correct: true}, //C
-        {d: "4", correct: false},
-        ]
-    }
+fetch('/public/quizData.json')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
 
-    ,
-    {
-        question: "Evaluate: ",
-        img: "/images/img_quiz/q2.png",
-        answer: [
-        {a: "-∞", correct: true}, //A
-        {b: "+∞", correct: false},
-        {c: "0", correct: false},
-        {d: "dne", correct: false},
-        ]
-    }
-    ,
-    {
-        question: "Evaluate: ",
-        img: "/images/img_quiz/q3.png",
-        answer: [
-        {a: "1/5", correct: false},
-        {b: "2/5", correct: true}, //B
-        {c: "3/5", correct: false},
-        {d: "4/5", correct: false},
-        ]
-    }
-    ,
-    {
-        question: "Find: ",
-        img: "/images/img_quiz/q4.png",
-        answer: [
-        {a: "2", correct: false},
-        {b: "3", correct: false},
-        {c: "4", correct: false},
-        {d: "5", correct: true}, //D
-        ]
-    }
-    ,
-    {
-        question: "Find: ",
-        img: "/images/img_quiz/q5.png",
-        answer: [
-        {a: "2", correct: false},
-        {b: "3", correct: true}, //B
-        {c: "4", correct: false},
-        {d: "5", correct: false},
-        ]
-    }
-    ,
-    {
-        question: "Evaluate: ",
-        img: "/images/img_quiz/q6.png",
-        answer: [
-        {a: "0", correct: true}, //A
-        {b: "1", correct: false},
-        {c: "2", correct: false},
-        {d: "3", correct: false},
-        ]
-    }
-    ,
-    {
-        question: "Evaluate: ",
-        img: "/images/img_quiz/q7.png",
-        answer: [
-        {a: "5/4", correct: false}, 
-        {b: "1/4", correct: true}, //B
-        {c: "3/4", correct: false},
-        {d: "7/4", correct: false},
-        ]
-    }
-    ,
-    {
-        question: "Evaluate: ",
-        img: "/images/img_quiz/q8.png",
-        answer: [
-        {a: "-√7/4", correct: false},
-        {b: "-√5/8", correct: false},
-        {c: "-√11/6", correct: false},
-        {d: "-√3/2", correct: true}, //D
-        ]
-    }
-    ,
-    {
-        question: "Evaluate: ",
-        img: "/images/img_quiz/q9.png",
-        answer: [
-        {a: "1/3", correct: false},
-        {b: "2/3", correct: true}, //B
-        {c: "9/10", correct: false},
-        {d: "9/5", correct: false},
-        ]
-    }
-    ,
-    {
-        question: "Evaluate: ",
-        img: "/images/img_quiz/q10.png",
-        answer: [
-        {a: "1", correct: true}, //A
-        {b: "2", correct: false},
-        {c: "3", correct: false},
-        {d: "4", correct: false},
-        ]
-    }
-    ,
-    {
-        question: "Find: ",
-        img: "/images/img_quiz/q11.png",
-        answer: [
-        {a: "0", correct: false},
-        {b: "1", correct: false},
-        {c: "2", correct: false},
-        {d: "dne", correct: true}, //D
-        ]
-    }
-    ,
-    {
-        question: "Find: ",
-        img: "/images/img_quiz/q12.png",
-        answer: [
-        {a: "dne", correct: false},
-        {b: "0", correct: true}, //B
-        {c: "1", correct: false},
-        {d: "2", correct: false},
-        ]
-    }
-    ,
-    {
-        question: "Find: ",
-        img: "/images/img_quiz/q13.png",
-        answer: [
-        {a: "-1", correct: false},
-        {b: "0", correct: false},
-        {c: "1", correct: true}, //C
-        {d: "2", correct: false},
-        ]
-    }
-    ,
-    {
-        question: "Find: ",
-        img: "/images/img_quiz/q14.png",
-        answer: [
-        {a: "-5", correct: false},
-        {b: "-4", correct: false},
-        {c: "-3", correct: false},
-        {d: "-2", correct: true}, //D
-        ]
-    }
-    ,
-    {
-        question: "Find: ",
-        img: "/images/img_quiz/q15.png",
-        answer: [
-        {a: "3", correct: true}, //A
-        {b: "2", correct: false},
-        {c: "1", correct: false},
-        {d: "0", correct: false},
-        ]
-    }
-    ,
-    {
-        question: "Evaluate: ",
-        img: "/images/img_quiz/q16.png",
-        answer: [
-        {a: "-5/6", correct: true}, //A
-        {b: "-1/4", correct: false},
-        {c: "-5/8", correct: false},
-        {d: "-7/9", correct: false},
-        ]
-    }
-    ,
-    {
-        question: "Evaluate: ",
-        img: "/images/img_quiz/q17.png",
-        answer: [
-        {a: "-∞", correct: false},
-        {b: "+∞", correct: false},
-        {c: "1", correct: false},
-        {d: "dne", correct: true}, //D
-        ]
-    }
-    ,
-    {
-        question: "Evaluate: ",
-        img: "/images/img_quiz/q18.png",
-        answer: [
-        {a: "-1/8", correct: true}, //A
-        {b: "-3/8", correct: false},
-        {c: "-5/8", correct: false},
-        {d: "-7/8", correct: false},
-        ]
-    }
-    ,
-    {
-        question: "Evaluate: ",
-        img: "/images/img_quiz/q19.png",
-        answer: [
-        {a: "3/7", correct: false},
-        {b: "-3/7", correct: false},
-        {c: "7/3", correct: true}, //C
-        {d: "-7/3", correct: false},
-        ]
-    }
-    ,
-    {
-        question: "Evaluate: ",
-        img: "/images/img_quiz/q20.png",
-        answer: [
-        {a: "1", correct: false},
-        {b: "2", correct: false},
-        {c: "3", correct: false},
-        {d: "dne", correct: true}, //D
-        ]
-    }
-    ,
-    
-];
+ 
 
-const quiz= document.getElementById('quiz')
-const answerEls = document.querySelectorAll('.answer')
+
+const quiz= document.getElementById('quiz');
+const answerEls = document.querySelectorAll('.answer');
 const quizContainer = document.querySelector(".quiz-container");
-const questionEl = document.querySelector('.quiz-container-header')
+const questionEl = document.querySelector('.quiz-container-header');
 const quizImage = document.getElementById("quiz-image");
-const a_text = document.getElementById('a_text')
-const b_text = document.getElementById('b_text')
-const c_text = document.getElementById('c_text')
-const d_text = document.getElementById('d_text')
-const promptId = document.querySelector(".prompt-card")
-const realQ = document.getElementById('real-quiz')
-const submitBtn = document.getElementById('submit')
+const scoreText = document.querySelector('.scoreText');
+const scoreCard = document.querySelector('.score-card');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const promptId = document.querySelector(".prompt-card");
+const realQ = document.getElementById('real-quiz');
+const submitBtn = document.getElementById('submit');
 
 
 let currentQuiz = 0
@@ -255,13 +40,13 @@ answerEls.forEach((answerEl) => {
   });
 
 
-quizImage.src = quizData[0].answer[0].img;
+quizImage.src = data[0].answer[0].img;
 
 
 function loadQuiz() {
     deselectAnswers()
     submitBtn.disabled = true;
-    const currentQuizData = quizData[currentQuiz]
+    const currentQuizData = data[currentQuiz]
     quizImage.src = currentQuizData.img;
     questionEl.innerText = `Question ${currentQuiz+1} ${currentQuizData.question}`
     a_text.innerText = currentQuizData.answer[0].a
@@ -280,7 +65,7 @@ function getSelected() {
     answerEls.forEach(answerEl => {
       if (answerEl.checked) {
         const answerId = answerEl.id;
-        const isCorrect = quizData[currentQuiz].answer.some(a => a[answerId] && a.correct);
+        const isCorrect = data[currentQuiz].answer.some(a => a[answerId] && a.correct);
         
         if (isCorrect) {
           answer = true;
@@ -306,20 +91,18 @@ submitBtn.addEventListener('click', () => {
            score++
        }
        currentQuiz++
-       if(currentQuiz < quizData.length) {
+       if(currentQuiz < data.length) {
            loadQuiz()
-           if(currentQuiz == (quizData.length-1)){
+           if(currentQuiz == (data.length-1)){
                 submitBtn.innerHTML = "See result";
            }
        } 
        else {
-           quiz.innerHTML = `
-           <h2>You answered ${score}/${quizData.length} questions correctly</h2>
-
-           <button onclick="location.reload()">Reload</button>
-           `
+            quizContainer.classList.remove('active');
+           scoreCard.classList.add('active')
+           scoreText.innerHTML = `You answered ${score}/${data.length} questions correctly`
        }
        
 })
 
-
+});
