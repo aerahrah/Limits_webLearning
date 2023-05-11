db332a530c5c8e8acc2f77a8ec1b140e380c84f6
Marc Ivan Guillermo <61181505+aerahrah@users.noreply.github.com>
2023-05-11 11:31:31 +0800

    Quick fix start.astro

--- src/scripts/start.js
+++ src/scripts/start.js
@@ -1,7 +1,7 @@
 const vidContainer = document.getElementById("vid-container");
 const mainContainer = document.getElementById("main-container");
 const acceptBtn = document.getElementById("accept-btn");
-const skipBtn = document.getElementById("start-btn");
+const skipBtn = document.getElementById("skip-btn");
 const overlayStart = document.getElementById("overlay-start");
 
 acceptBtn.addEventListener("click", () => {
